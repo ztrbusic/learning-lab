a809a4f8d7af6638f5ed4e915ca72f4206a98313
Zeljko Trbusic <ztrbusic@gmail.com>
2025-06-08 20:32:57 +0200

    Learning

---
+++ 008-elif2.py
@@ -0,0 +1,12 @@
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+if name == 'Melita':
+	print('Bok Melita')
+elif age == 1:
+	print('Age 1')
+elif age < 10:
+	print ('Age less than 10')
+elif age > 10:
+	print('Age more than 10')
+elif age > 1000:
+	print('Age more than 1000')
---
+++ 009-while1.py
@@ -0,0 +1,4 @@
+age = int(input())
+while age < 5:
+	print('Hi')
+	age = age +1
