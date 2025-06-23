3fc8f52824621b8b7705c910697cfe0ce7bafc2f
Zeljko Trbusic <ztrbusic@gmail.com>
2025-06-23 18:40:18 +0200

    Create 028-random-list.py

---
+++ 028-random-list.py
@@ -0,0 +1,7 @@
+import random
+
+messages = ["It is certain", "it is decidedly so", "Yes, definitely",
+"Reply hazy, try again", "Ask again later", "Concentrate and try again",
+"My reply is no", "outlook not so good", "Very doubtful"]
+
+print(random.choice(messages))
