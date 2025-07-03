ade047bc28533fb3186c776b621c6c25c766a1f1
Zeljko Trbusic <ztrbusic@gmail.com>
2025-07-03 13:23:13 +0200

    Update 039-book-catalogue.py

--- code/039-book-catalogue.py
+++ code/039-book-catalogue.py
@@ -1,10 +1,22 @@
-inventory = {}
+# Adding new shelf
+def add_shelf(shelfname):
+	shelves = {}
+	
 
-def add_shelf(inventory):
-	shelf_name = input("Write a new for your new shelf:")
-	if shelf_name in inventory:
-		print("Shelf name already exists.")
-	else:
-		inventory[shelf_name] = []
-		print("Shelf " + shelf_name + " added.")
 
+
+
+
+
+
+
+
+
+
+# Adding new book to shelf
+
+# Searching for book
+
+# Listing books in shelf
+
+# 
