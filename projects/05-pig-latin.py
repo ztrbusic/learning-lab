9dbe8c74d652ba146a793821c2cdc7e30b9a1ff9
Zeljko Trbusic <ztrbusic@gmail.com>
2025-07-10 22:09:01 +0200

    Update project programs

---
+++ projects/05-pig-latin.py
@@ -0,0 +1,7 @@
+# English to Pig latin
+print("Enter the English message to translate into Pig Latin:")
+message = input()
+
+vowels = ("a", "e", "i", "o", "u", "y")
+
+pig_latin = [] # A list of the words in Pig Latin.
