6fac17fff47757cddca8f906d3e94a5d09719376
Zeljko Trbusic <ztrbusic@gmail.com>
2025-07-11 09:10:59 +0200

    Delete 05-pig-latin.py

--- projects/05-pig-latin.py
+++
@@ -1,7 +0,0 @@
-# English to Pig latin
-print("Enter the English message to translate into Pig Latin:")
-message = input()
-
-vowels = ("a", "e", "i", "o", "u", "y")
-
-pig_latin = [] # A list of the words in Pig Latin.
