71be7cfb6d7018b55b1a6dfb1bb17e22bf5267d0
Zeljko Trbusic <ztrbusic@gmail.com>
2025-07-25 15:08:18 +0200

    Update 05-rpg-game.py

--- projects/05-rpg-game.py
+++ projects/05-rpg-game.py
@@ -1,6 +1,7 @@
-import time, sys
+import time, sys, textwrap
 from colorama import init, Fore, Style
 
+#Function for printing text slowly
 def slow_print(text, delay):
 	for char in text:
 		sys.stdout.write(char)
@@ -8,28 +9,32 @@ def slow_print(text, delay):
 		time.sleep(delay)
 	print()
 
-slow_print(Fore.MAGENTA + "Simple RPG" + Style.RESET_ALL, 0.1)
-slow_print("""
-This is a simple RPG game.
-You are a fierce warrior entering a dungeon.
-You goal is to make simple choices to get to the end where you will have
-three questions to answer.
-If you answer the questions correctly, you will find out the meaning of life.
-""", 0.1)
-
+#Player and enemy stats
 player = {"name": "", "health": 100, "damage": 10}
 enemy_room_1 = {"name": "Dungeon Troll", "health": 50000, "damage": 1000}
-room_1 = {"description": "This is the first room. A troll lingers inside", "visited": False}
+room_1 = {"description": "This is the first room. A troll lingers inside"}
 
+#Intro
+slow_print(Fore.MAGENTA + "Simple RPG" + Style.RESET_ALL, 0.1)
+slow_print(textwrap.dedent(f"""
+			This is a simple RPG game.
+			You are a fierce warrior entering a dungeon.
+			Your goal is to make simple choices to get to the end
+			where you will have three questions to answer.
+			If you answer the questions correctly, you will find out the meaning of {Fore.YELLOW}life{Style.RESET_ALL}.
+			"""), 0.1)
+
+#Player initialization
 slow_print("Input player name: ", 0.1)
 player["name"] = input()
 slow_print("Hi " + player["name"] + ". These are your stats:", 0.1)
-slow_print(f"""
-Name: {player['name']}
-Health: {player['health']}
-Damage: {player['damage']}
-""", 0.05)
+slow_print(textwrap.dedent(f"""
+			Name: {player['name']}
+			Health: {player['health']}
+			Damage: {player['damage']}
+			"""), 0.05)
 
+#Entrance to the first room
 while True:
 	slow_print("Press " + Fore.BLACK + "any key " + Style.RESET_ALL + "to continue to the first room: ", 0.1)
 	for attempt in range(4):
@@ -45,41 +50,49 @@ while True:
 		if user_input == "any key":
 			break
 	break
-	
+
+#Room 1
 slow_print(room_1["description"], 0.1)
 time.sleep(3)
-slow_print("This is the enemy and his stats:", 0.05)
-slow_print(f"""
-Enemy name: {enemy_room_1['name']}
-Enemy health: {enemy_room_1['health']}
-Enemy damage: {enemy_room_1['damage']}
-""", 0.1)
+slow_print("This is the enemy and his stats:", 0.1)
+slow_print(textwrap.dedent(f"""
+			Enemy name: {enemy_room_1['name']}
+			Enemy health: {enemy_room_1['health']}
+			Enemy damage: {enemy_room_1['damage']}
+			"""), 0.05)
 slow_print("Hmmmm... This enemy seems unbeatable. You have just started on your journey, it would be a shame to die so early.", 0.1)
-slow_print(	"""
-You have two choices:
-1. Fight,
-2. Run.
-Which one will it be?
-""", 0.1)
+slow_print(textwrap.dedent("""
+			You have two choices:
+			1. Fight,
+			2. Run.
+			Which one will it be?
+			"""), 0.1)
 choice_room_1 = input()
 if choice_room_1 == "1" or choice_room_1 == "1." or choice_room_1 == "Fight" or choice_room_1 == "1. Fight":
 	enemy_room_1["health"] -= 10
-	slow_print(f"""
-You deal 10 damage to the enemy.
-{enemy_room_1['name']} has {enemy_room_1['health']} health left.
-Enemy turn. {enemy_room_1['name']} deals {enemy_room_1['damage']} damage to {player['name']} .
+	slow_print(textwrap.dedent(f"""
+			You deal 10 damage to the enemy.
+			{enemy_room_1['name']} has {enemy_room_1['health']} health left.
+			Enemy turn. {enemy_room_1['name']} deals {enemy_room_1['damage']} damage to {player['name']} .
 
-You are dead.
+			You are dead.
 
-Game over. 
-""", 0.1)
+			Game over.
+			"""), 0.1)
 	sys.exit()
 elif choice_room_1 == "2" or choice_room_1 == "2." or choice_room_1 == "Run" or choice_room_1 == "2. Run":
-	slow_print("You decided to run. Good. Your decision was wise, the enemy was too difficult for you, "
-	f"{player['name']}. But, in your attempt to flee quickly, you have fallen into a basement...", 0.1)
+	slow_print(textwrap.dedent(f"""
+			You decided to run. Good. Your decision was wise, the enemy was too difficult for you, {player['name']}.
+			But, in your attempt to flee quickly, you have fallen into a basement...
+			"""), 0.1)
 else:
 	slow_print("Don't play games with me.", 0.5)
 	sys.exit()
+
+#Room 2 - (need to define variable and make player find a key to leave)
+
+
+
 
 
 
