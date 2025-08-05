a7575743d6e5eee65f092d5433307f43c1f79fb2
Zeljko Trbusic <ztrbusic@gmail.com>
2025-08-05 17:55:26 +0200

    Create 051-phone-and-email.py

---
+++ learning-code/051-phone-and-email.py
@@ -0,0 +1,38 @@
+#! python3
+# phone-and-email.py - Finds phone numbers and email addresses on the clipboard.
+
+import pyperclip, re
+
+phone_regex = re.compile(r"""(
+			(\d{3}|\(\d{3}\)|\d{2})?		# area code
+			(\s|-|\.|/)?					# separator
+			(\d{4})							# first 4 digits
+			(\s|-|\.)						# separator
+			(\d{3})							# last 3 digits
+			(\s*(ext|x|ext.)\s*(\d{2,5}))?	# extension
+			)""", re.VERBOSE)
+
+email_regex = re.compile(r"""(
+	[a-zA-Z0-9._%+-]+		# username
+	@						# @ symbol	
+	[a-zA-Z0-9.-]+			# domain name
+	(\.[a-zA-Z]{2,4})		# dot-something
+	)""", re.VERBOSE)
+
+text = str(pyperclip.paste())
+
+matches = []
+for groups in phone_regex.findall(text):
+	phone_num = '-'.join([groups[1], groups[3], groups[5]])
+	if groups[8] != '':
+		phone_num += ' x' + groups[8]
+	matches.append(phone_num)
+for groups in email_regex.findall(text):
+	matches.append(groups[0])
+
+if len(matches) > 0:
+	pyperclip.copy("\n".join(matches))
+	print("Copied to clipboard:")
+	print("\n".join(matches))
+else:
+	print("No phone numbers or email addresses found.")
