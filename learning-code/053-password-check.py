7d798b2f33e20f4cb89244ff4193b3e1c5c29af2
Zeljko Trbusic <ztrbusic@gmail.com>
2025-08-27 18:21:59 +0200

    Update 053-password-check.py

--- learning-code/053-password-check.py
+++ learning-code/053-password-check.py
@@ -2,8 +2,12 @@ import re
 
 def strong_pass_check(password):
 	if not isinstance (password, str):
-		return False
+		return "Its not string"
 	if len(password) < 8:
-		return False
-		print("Min 8 characters")
-	if re.search
+		return "Min 8 characters"
+	if not re.search(r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$", password):
+		return "Wrong password"
+
+password = input()
+result = strong_pass_check(password)
+print(result)
