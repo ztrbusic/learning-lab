4591cf32f87dba754b94c66e15bdcb8ca24b4db4
Zeljko Trbusic <ztrbusic@gmail.com>
2025-08-29 10:33:12 +0200

    Update 054-strip-regex.py

--- learning-code/054-strip-regex.py
+++ learning-code/054-strip-regex.py
@@ -1,9 +1,17 @@
 import re
 
-def strip(string, chars=None):
-	if chars == None:
+def my_strip(string, chars=None):
+	if chars is None:
 		string = re.sub(r"^\s+", "", string)
 		string = re.sub(r"\s+$", "", string)
+	elif chars == "":
+		return string
 	else:
-		# nastaviti ovdje
+		pattern = f"[{re.escape(chars)}]"
+		string = re.sub(f"^{pattern}+", "", string)
+		string = re.sub(f"{pattern}+$", "", string)
 	return string
+
+string_out = input()
+chars_out = input()
+print(my_strip(string_out, chars_out))
